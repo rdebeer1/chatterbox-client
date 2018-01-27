00d0cfbbf21438e951764d1c1df78cb509987c66
rdebeer1 <Robert.DeBeer@gmail.com>
2018-01-26 19:38:38 -0600

    Builds v1.0

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,85 @@
-// YOUR CODE HERE:
+var app = {
+  server: 'http://parse.atx.hackreactor.com/chatterbox/classes/messages',
+  currentRoom: 'lobby',
+  context: this,
+  messages: [],
+  messagesShown: 0,
+  
+  
+  
+  init: () => {
+    $('#new-message').click(() => {
+      $('#message-form').css('display', 'block');
+    });
+    $('#submit').click(() => {
+      console.log($('#message-box').val());
+      app.send($('#message-box').val());
+    });
+    $('#alerts').click(() => {
+      app.renderMessage(app.messages[app.messages.length - 1 - app.messagesShown]);
+      app.messagesShown++;
+      app.fetch();
+      $('#alerts').text('Alerts');
+    });
+    $('#clear').click(() => {
+      app.clearMessages();
+    });
+    $('#add-room').click(() => {
+      app.renderRoom($('#new-room').val());
+      $('#new-room').val('');
+    });
+  },
+  
+  fetch: () => {
+    $.get(app.server, response => {
+      if (app.messages.length === 0) {
+        _.each(response.results, (message) => {
+          app.messages.push(message);
+        });
+        setInterval(app.fetch, 5000);
+      } else {
+        $('#alerts').text('New messages!');
+      }
+    }); 
+  },
+  
+  send: (message) => {
+    $.ajax({
+      url: app.server,
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: (data) => {
+        alert('Message sent!');
+      },
+      error: (data) => {
+        alert('Message failed to send');
+      }
+    });
+  },
+  
+  renderMessage: message => {
+    // for (let i = app.messagesShown; i < app.messagesShown + 20; i++) {
+    //   let message = app.messages[i];
+    //   if (message === undefined) {
+    //     return;
+    //   }
+    $('#chats').append(`<div class="post"><div class="post-header"><a href="#">${message.username}</a>, ${new Date(message.createdAt)}</div><div class="post-body">${message.text}</div></div>`);
+    // }
+    // app.messagesShown += 20;
+  },
+  
+  clearMessages: () => {
+    $('#chats').html('');
+  },
+
+  
+  renderRoom: (room) => {
+    $('#roomSelect').append(`<option value = '${room}'>${room}</option>`);
+  }
+};
+
+$(document).ready(() => {
+  app.init();
+  app.fetch();
+});
